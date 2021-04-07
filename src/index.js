94c6fc75c66811f2943812f0ebb406e2a777c7dd
maximiliano garcia <maximiliano_garcia93@hotmail.com>
2021-04-06 22:20:15 -0300

    creacion del projecto

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from "firebase/app";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+firebase.initializeApp({
+  apiKey: "AIzaSyAyXxQijG4eoHvsHEPyPCJTi26roSRXupc",
+  authDomain: "truchigram.firebaseapp.com",
+  databaseURL: "https://truchigram.firebaseio.com",
+  projectId: "truchigram",
+  storageBucket: "truchigram.appspot.com",
+  messagingSenderId: "887923179137",
+  appId: "1:887923179137:web:b8ddab7c7f929486"
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
